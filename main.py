74224c1b30cd9da1f1004d6e670117f14910b988
Inferlopec <ilja.golubevs@gmail.com>
2025-03-20 22:03:04 +0200

    Screen output base, pygame.init

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+
+
+if __name__ == "__main__":
+    main()
