da496daff5bfde6f5b2a6b2b66279d761b47de5e
barovig <semargl1349@gmail.com>
2025-03-21 16:57:07 +0200

    Increased target frame rate to 144Hz

--- main.py
+++ main.py
@@ -69,8 +69,8 @@ def main():
 
         pygame.display.flip()
 
-        # Limit frame rate to 60 fps
-        dt = clock.tick(60) / 1000
+        # Limit frame rate to 144 fps
+        dt = clock.tick(144) / 1000
 
 if __name__ == "__main__":
     main()
