73fb5f34946e2cbd78997a188254aeb45174ad12
Inferlopec <ilja.golubevs@gmail.com>
2025-03-21 19:55:43 +0200

    New features:
    1. Highscore system
    
    Changelog:
    
    -Separate class for handling hud and gui elements
    
    -Changing main loop code logic
    
    -Changing reverse movement logic
    
    -Overall code rafactoring for better readibillity

--- constants.py
+++ constants.py
@@ -1,13 +1,25 @@
+# Screen dimensions
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
+# Asteroid properties
 ASTEROID_MIN_RADIUS = 20
 ASTEROID_KINDS = 3
 ASTEROID_SPAWN_RATE = 0.8  # seconds
 ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+
+# Player properties
 PLAYER_RADIUS = 20
 PLAYER_TURN_SPEED = 300  # degrees per second
 PLAYER_SPEED = 200  # pixels per second
+PLAYER_SHOOT_COOLDOWN = 0.3  # seconds
+PLAYER_INVINCIBILITY_TIME = 3.0  # seconds after respawn
+
+# Shot properties
 SHOT_RADIUS = 5
 PLAYER_SHOT_SPEED = 500  # pixels per second
-PLAYER_SHOOT_COOLDOWN = 0.3  # seconds
+
+# Score values
+SCORE_LARGE = 20   
+SCORE_MEDIUM = 50  
+SCORE_SMALL = 100
